356a2162aab032fce0f80fcb4a41d01029cbf61e
Bikrant Jung <bikrantjung1214@gmail.com>
2023-04-01 10:13:41 +0545

    Setup React query

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App, { router } from "./App";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Home from "./components/home/Home.page";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
