670f7745350cb32b994610989dae58f16b602969
Bikrant Jung <bikrantjung1214@gmail.com>
2023-04-01 09:57:43 +0545

    Basic navbar styling

--- src/components/home/Home.page.tsx
+++ src/components/home/Home.page.tsx
@@ -2,23 +2,27 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
 function Home() {
+  const className =
+    "bg-white transition p-2 hover:bg-gray-600 hover:text-white";
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
+      <nav className="bg-gray-800 text-gray-900 p-3">
+        <ul className="flex gap-2 items-center">
+          <li className={className}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className={className}>
             <Link to="/traditional-fetch">Traditional Fetch</Link>
           </li>
-          <li>
+          <li className={className}>
             <Link to="/react-query-fetch">React Query Fetch</Link>
           </li>
         </ul>
       </nav>
       <div>
-        <h1>Welcome to React Query Class</h1>
+        <h1 className="text-4xl text-center font-serif mt-3">
+          Welcome to React Query Class
+        </h1>
       </div>
       <div>
         <Outlet />
