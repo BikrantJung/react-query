de64ecb6e351976c07b0b82e6ff9a6539a81b346
Bikrant Jung <bikrantjung1214@gmail.com>
2023-04-01 09:51:42 +0545

    Setup tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App, { router } from "./App";
 import Home from "./components/home/Home.page";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
